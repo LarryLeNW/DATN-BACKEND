c3b047ed5d693a518120185b198c669f75ac1a0d
larrylenw <larrylenw@gmail.com>
2024-11-12 20:56:31 +0700

    resolve db

--- src/main/java/com/backend/configuration/DataSeeder.java
+++ src/main/java/com/backend/configuration/DataSeeder.java
@@ -28,6 +28,8 @@ public class DataSeeder {
 	List<Permission> permissions = new ArrayList<>(Arrays.asList(new Permission((long) 1, "CREATE"),
 			new Permission((long) 2, "VIEWALL"), new Permission((long) 3, "VIEW"), new Permission((long) 4, "UPDATE"),
 			new Permission((long) 5, "DELETE")));
+	
+	
 
 //	@PostConstruct
 	public void seed() {
